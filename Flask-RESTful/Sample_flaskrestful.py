115a1c64642b688cf9f1d1c02b21606fe0f5849c
akankshahinge <70201851+akankshahinge@users.noreply.github.com>
2020-09-12 16:04:43 +0530

    Update Sample_flaskrestful.py

--- Flask-RESTful/Sample_flaskrestful.py
+++ Flask-RESTful/Sample_flaskrestful.py
@@ -23,6 +23,21 @@ class Item(Resource):
     items.append(item)
     return item, 201
   
+  def delete(self, name):
+    global items
+    items=list(filter(lamda x:x['name']!=name,items))
+    return {'message':'Item deleted'}
+  
+  def put(self,name):
+    data=request.get_json()
+    item=next(filter(lamda x:x['name']==name,items),None)
+    if item is None:
+      item={'name':name,'price':data['price']}
+      items.append(item)
+    else: 
+      item.update(data)
+    
+  
 class ItemList(Resource):
   def get(self):
     return {'items':items}
